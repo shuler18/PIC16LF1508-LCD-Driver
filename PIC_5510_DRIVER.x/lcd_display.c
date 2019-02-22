1d06d0fd20ce74a09580a70a2ed24e01b0a672fd
shuler18 <shuler18@up.edu>
2019-02-22 00:21:20 -0800

    Shrank Table
    Working to fit table in RAM, so I shrank it down to the bare necessities for now.
    
    Starting to add some drawing stuff and organizing the code nicely

--- PIC_5510_DRIVER.x/lcd_display.c
+++ PIC_5510_DRIVER.x/lcd_display.c
@@ -1,6 +1,7 @@
 /*
- * File:   newmain.c
- * Author: shuler18
+ * File:   lcd_display.c
+ * Author: Dylan Shuler
+ * Email: dylanshuler11@gmail.com
  *
  * Created on February 7, 2019, 3:39 PM
  */
@@ -35,7 +36,8 @@
 #include <stdint.h>
 #include "lcd_display.h"
 #include "SPI_comms.h"
-#include "nokiaFont.h"
+#include "nokiaFont.h" //comment this out if it takes too much memory
+
 
 void main(void) {
     OSCCONbits.IRCF=0x0; //start lf 31khz clk
@@ -55,18 +57,12 @@ void main(void) {
         fill_lcd(0x0F); //put some crap on screen, should be half of each col
         __delay_ms(500);
         fill_lcd(0x0);
+		sendLCDstring("hello world");
     }
 }
 
-/*
- * function to set cursor to specific row and write data to it
- */
-void set_pixel(char x, char y, char data)
-{
-    set_x(x);
-    set_y(y);
-    SPI_write_data(data);
-}
+
+// ******************* init and utility functions ******************* //
 
 /*
  * Function to send LCD commands to LCD via SPI bus
@@ -77,13 +73,15 @@ void sendLCDcmd(char cmd)
     SPI_write_data(cmd);
 }
 
-
+/*
+ *send LCD a single character using font table
+ */
 void sendLCDdata(char data)
 {
     int i;
     for(i = 0; i < 5; i++)
     {
-        //SPI_write_data(font[(data) - (0x20)[i]]);
+        SPI_write_data(font[(data) - (0x20)[i]]);
     }
 }
 
@@ -93,37 +91,20 @@ void sendLCDdata(char data)
 void init_5510(void)
 {
     //reset LCD on startup. Takes t(ms)>100
-    __delay_ms(100); //likely not needed, but ensures the negative edge is seen
+    __delay_ms(10); //likely not needed, but ensures the negative edge is seen
     RST = 0;
-    __delay_ms(500);
+    __delay_ms(150);
     RST = 1;
     
     //lcd init commands
     sendLCDcmd(0x21); //extended cmd's follow
     sendLCDcmd(0xC0); //set Vop aka contrast, this was the best value found
     sendLCDcmd(0x07); //set tempature coef
-    sendLCDcmd(0x13); //set LCD Bias to 1:65 may be wrong/unneeded
-    sendLCDcmd(0x20); //must send 0x20 before modding display control mode
+    sendLCDcmd(0x13); //set LCD Bias to 1:65 maybe try 1:48?
+    sendLCDcmd(0x20); //must send 0x20 to switch back to normal cmd mode
     sendLCDcmd(0x0C); //normal display mode
 }
 
-/*
- *function to fill lcd screen with a single value
- * 
- * int wb - 0=white ff = black
- */
-void fill_lcd(char wb)
-{
-    int i, j;
-    DC = 1; //sending data
-    for(i=0; i<6; i++)
-    {
-        for(j=0; j<84; j++)
-        {
-            SPI_write_data(wb);
-        }
-    }
-}
 
 /*
  * function to set the position of both X and Y cursors
@@ -148,4 +129,54 @@ void set_x(char x)
 void set_y(char y)
 {
     sendLCDcmd(y | 0x40);
+}
+
+
+// ******************* drawing functions ******************* //
+
+/*
+ *function to fill lcd screen with a single value
+ * 
+ * int wb - 0=white ff = black
+ */
+void fill_lcd(char wb)
+{
+    int i, j;
+    DC = 1; //sending data
+    for(i=0; i<6; i++)
+    {
+        for(j=0; j<84; j++)
+        {
+            SPI_write_data(wb);
+        }
+    }
+}
+
+
+/*
+ *send LCD a a string using the font table
+ */
+void sendLCDstring(char *str) {
+	while((*str)!=0)
+    {   
+        SPI_write_data(*str);
+        str++;
+    }
+}
+
+/*
+ * function to set cursor to specific row and write data to it
+ */
+void set_pixel(char x, char y, char data)
+{
+    set_x(x);
+    set_y(y);
+    SPI_write_data(data);
+}
+
+void draw_rectangle(char length, char width, int thickness)
+{
+	int i, j;
+	set_x(x);
+	
 }
